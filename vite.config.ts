1f9b32d1af3a807cb1fd43a0276aae6ad00920e2
bryzZz <nikita.tebloev@polyus-nt.ru>
2023-08-24 15:47:44 +0800

    add workspace

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/transcript-admin/",
+  base: "/policy-admin/",
   plugins: [svgr(), react()],
   resolve: {
     alias: {
